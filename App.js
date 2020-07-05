08c25a6bda780a5bd08f7de1e81f149ac9bc4183
lishu gupta <lishugupta652@gmail.com>
2020-07-04 23:54:44 -0400

    animation

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Asset } from "expo";
+import { Asset } from "expo-asset";
+import { AppLoading } from "expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>JMAdsfian worSFsdfking</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import JmaApp from "./app/index";
+
+function cacheImages(images) {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([require("./assets/bg.jpg")]);
+
+    await Promise.all([...imageAssets]);
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    return <JmaApp />;
+  }
 }
 
 const styles = StyleSheet.create({
